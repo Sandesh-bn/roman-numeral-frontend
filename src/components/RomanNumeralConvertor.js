ecefeb26c081ee93301d5bfa7f8cbfbafc407866
Sandesh-bn <sandesh647bn@gmail.com>
2025-07-21 21:06:47 -0700

    Replace url with vercel url

--- src/components/RomanNumeralConvertor.js
+++ src/components/RomanNumeralConvertor.js
@@ -1,9 +1,8 @@
 import { Button, defaultTheme, Provider } from '@adobe/react-spectrum';
-import { TextField, Heading, Text, Grid } from '@adobe/react-spectrum';
+import { TextField, Heading, Text, Grid, View } from '@adobe/react-spectrum';
 import { useState } from 'react';
 import Moon from '@spectrum-icons/workflow/Moon';
 import Light from '@spectrum-icons/workflow/Light';
-
 
 export function RomanNumeralConvertor() {
     const [romanNumeral, setRomanNumeral] = useState('');
@@ -13,18 +12,27 @@ export function RomanNumeralConvertor() {
     const [colorScheme, setColorScheme] = useState('light');
 
     function handleSubmit() {
-        fetch("http://localhost:8080/romannumeral?query=" + userInput)
-            .then(res => res.json())
-            .then(
-                (result) => {
-                    setIsLoading(false);
-                    setRomanNumeral(result.output);
-                },
-                (error) => {
-                    setIsLoading(false);
-                    setError(error);
-                }
-            )
+        let integer = parseInt(userInput);
+
+        if (integer > 0 && integer < 4000) {
+            // fetch("http://localhost:8080/romannumeral?query=" + userInput)
+            fetch("https://roman-number-backend.vercel.app/romannumeral?query=" + userInput)
+                .then(res => res.json())
+                .then(
+                    (result) => {
+                        setIsLoading(false);
+                        setRomanNumeral(result.output);
+                    },
+                    (error) => {
+                        setIsLoading(false);
+                        setError(error);
+                    }
+                )
+        }
+        else {
+            setRomanNumeral('')
+        }
+
     }
 
     function handleKeyDown(e) {
@@ -34,10 +42,11 @@ export function RomanNumeralConvertor() {
 
     function handleChange(inputValue) {
         setUserInput(inputValue);
+        setRomanNumeral('')
         const numericValue = parseInt(inputValue, 10);
 
         if (inputValue === '') {
-            setError(null); 
+            setError(null);
         } else if (isNaN(numericValue)) {
             setError('Please enter a valid number.');
         }
@@ -62,38 +71,39 @@ export function RomanNumeralConvertor() {
             : 'valid';
 
     return (
-        <Provider height={'100vh'} colorScheme={colorScheme} theme={defaultTheme}>
+        <Provider colorScheme={colorScheme} theme={defaultTheme}>
+            <View padding={'size-100'} height={'100vh'} >
+                {colorScheme == 'light' ?
+                    <div onClick={() => toggleColorScheme()}>
+                        <Moon aria-label="Toggle Color Scheme" />
+                    </div> :
+                    <div onClick={() => toggleColorScheme()}>
+                        <Light aria-label="Toggle Color Scheme" />
+                    </div>
+                }
 
-            {colorScheme == 'light' ?
-                <div onClick={() => toggleColorScheme()}>
-                    <Moon aria-label="Toggle Color Scheme" />
-                </div> :
-                <div onClick={() => toggleColorScheme()}>
-                    <Light aria-label="Toggle Color Scheme" />
-                </div>
-            }
-
-            <Grid justifyContent={'center'} height="50%">
-                <Heading level={1}>Roman numeral convertor</Heading>
-                <TextField
-                    type="number"
-                    label="Enter a number"
-                    value={userInput}
-                    onKeyDown={handleKeyDown}
-                    onChange={handleChange}
-                    validationState={validationState}
-                    errorMessage={errorMessage}
-                    isRequired
-                />
-                <br />
-                <Button
-                    variant="primary"
-                    onPress={() => handleSubmit()}
-                >
-                    Convert to roman numeral
-                </Button>
-                <Heading level={4}>Roman numeral: </Heading><Text>{romanNumeral}</Text>
-            </Grid>
+                <Grid justifyContent={'center'} height="50%">
+                    <Heading level={1}>Roman numeral convertor</Heading>
+                    <TextField
+                        type="number"
+                        label="Enter a number"
+                        value={userInput}
+                        onKeyDown={handleKeyDown}
+                        onChange={handleChange}
+                        validationState={validationState}
+                        errorMessage={errorMessage}
+                        isRequired
+                    />
+                    <br />
+                    <Button
+                        variant="primary"
+                        onPress={() => handleSubmit()}
+                    >
+                        Convert to roman numeral
+                    </Button>
+                    <Heading level={4}>Roman numeral: </Heading><Text>{romanNumeral}</Text>
+                </Grid>
+            </View>
         </Provider>
     );
 }
